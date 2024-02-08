a3cb9e0eba7605e87774469b357cd8795ba804f8
Shivam verma <shivamverma@Shivams-MacBook-Air-2.local>
2024-02-08 11:28:16 +0530

    Password

--- Unit2/GUI/src/gui/MyTextField.java
+++ Unit2/GUI/src/gui/MyTextField.java
@@ -12,6 +12,7 @@ public class MyTextField {
         Container container=jf.getContentPane();
 
         container.setLayout(null);
+        container.setBackground(Color.gray);
 
         Font font=new Font("Helvetica",Font.ITALIC,10);
 
@@ -21,7 +22,28 @@ public class MyTextField {
         jTextField.setText("Enter username...");
         jTextField.setFont(font);
 
+        jTextField.setForeground(Color.WHITE);
+        jTextField.setBackground(Color.DARK_GRAY);
+
+        jTextField.setEditable(false);// by default true means we can edit it
+
         container.add(jTextField);
+
+        //Password
+        JPasswordField jPasswordField=new JPasswordField();
+        jPasswordField.setBounds(200,300,300,50);
+
+        jPasswordField.setText("123456");
+        jPasswordField.setFont(font);
+
+        jPasswordField.setForeground(Color.WHITE);
+        jPasswordField.setBackground(Color.DARK_GRAY);
+
+        jPasswordField.setEchoChar('#');//to change the type of char
+        jPasswordField.setEchoChar((char)0);//if you want to show the password
+
+        container.add(jPasswordField);
+
         jf.setVisible(true);
     }
 }
